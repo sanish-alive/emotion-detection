64c94dc8044aff92463137a623e83571d988d5ce
ssanish321@gmail.com <50664442+mostanglan@users.noreply.github.com>
2024-11-07 11:07:07 +0545

    'emotions'

--- src/emotions.py
+++ src/emotions.py
@@ -127,7 +127,7 @@ elif mode == "display":
             maxindex = int(np.argmax(prediction))
             cv2.putText(frame, emotion_dict[maxindex], (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
-        cv2.imshow('Video', cv2.resize(frame,(1600,960),interpolation = cv2.INTER_CUBIC))
+        cv2.imshow('Video', cv2.resize(frame,(1280,720),interpolation = cv2.INTER_CUBIC))
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
